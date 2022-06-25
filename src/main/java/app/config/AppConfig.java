04e4ba7a680c6f6ad37a654bf78e0b29b68ab87c
Pshukov Muhamed <46082231+MishaPsh@users.noreply.github.com>
2022-06-25 23:09:10 +0300

    Add files via upload

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,37 @@
+package app.config;
+
+import app.model.AnimalsCage;
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean("animalsCage")
+    public AnimalsCage getAnimalCage() {
+        return new AnimalsCage();
+    }
+
+    @Bean("cat")
+    public Cat getCat() {
+        return new Cat();
+    }
+
+    @Bean("dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+
+    @Bean("timer")
+    @Scope("singleton")
+    public Timer getTimer() {
+        return new Timer();
+    }
+
+}
